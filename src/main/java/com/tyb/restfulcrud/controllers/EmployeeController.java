cb0c453d67d3b2ed68b89f55ae76fb47e1e3d02f
root <>
2021-02-22 18:40:47 +0800

    V-1.0

--- src/main/java/com/tyb/restfulcrud/controllers/EmployeeController.java
+++ src/main/java/com/tyb/restfulcrud/controllers/EmployeeController.java
@@ -1,9 +1,13 @@
 package com.tyb.restfulcrud.controllers;
 
 import com.tyb.restfulcrud.dao.EmployeeDao;
+import com.tyb.restfulcrud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class EmployeeController {
@@ -11,7 +15,11 @@ public class EmployeeController {
     EmployeeDao employeeDao;
     //查询所有员工返回列表页面
     @GetMapping("/emps")
-    public String list(){
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+
+        //放在请求域中
+        model.addAttribute("emps", employees);
         //thymeleaf 默认自动拼串-> 到 classpath:/templates/ 寻找/emp/list.html 页面
         return "/emp/list";
     }
