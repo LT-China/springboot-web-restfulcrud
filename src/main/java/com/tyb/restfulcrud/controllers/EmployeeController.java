f12ba36e20810f937c4d92650ded8a5ece897ae4
root <>
2021-02-25 18:46:09 +0800

    V-1.0

--- src/main/java/com/tyb/restfulcrud/controllers/EmployeeController.java
+++ src/main/java/com/tyb/restfulcrud/controllers/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.tyb.restfulcrud.controllers;
 
+import com.tyb.restfulcrud.dao.DepartmentDao;
 import com.tyb.restfulcrud.dao.EmployeeDao;
+import com.tyb.restfulcrud.entities.Department;
 import com.tyb.restfulcrud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -13,6 +16,9 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
+
     //查询所有员工返回列表页面
     @GetMapping("/emps")
     public String list(Model model){
@@ -24,9 +30,21 @@ public class EmployeeController {
         return "/emp/list";
     }
 
-    //添加employee
-    @GetMapping("/empAdd")
-    public String toAddEmployee(){
+    //添加employee页面
+    @GetMapping("/empAddPage")
+    public String toAddEmployeePage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("deptInfo", departments);
+
         return "/emp/addEmployee";
     }
+
+    @PostMapping("/empAdd")
+    public String addEmp(Employee employee){
+        System.out.println(employee);
+        //redirect 重定向一个地址
+        //forward 转发到一个地址
+        return "redirect:/emps";
+    }
+
 }
